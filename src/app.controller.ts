f5634043662091a77c8a61492caf3a0030d38bbb
Claudio Vasquez <claudio.vasquez@unah.hn>
2023-05-06 20:34:08 -0600

    edits para explicar a alan

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -21,7 +21,7 @@ export class AppController {
     console.log("Packet: ", context.getPacket());
     this.appService.sumDataService(payload); //esto manda el payload(mensaje) hacia el servicio(funcion) que se creo en app.service.ts (ahora ver comentarios en app.service.ts)
   }
-
+  
   @MessagePattern('ftf-output') //como en app.service.ts se manda el mensaje al topico ftf-output, aqui se recibe el mensaje
   logData(@Payload() payload: string, @Ctx() context: MqttContext): string {
     console.log(`___New message ${context.getTopic()}___`); //esto imprime en consola el topico del mensaje
@@ -29,8 +29,4 @@ export class AppController {
     console.log("Packet: ", context.getPacket());
     return payload+ `response from logdata() in -t ${context.getTopic()}`; //esto regresa el mensaje que se recibio
   }
-
-  
-  
-  
 }
